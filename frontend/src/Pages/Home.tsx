019c16b162985928c4975fb37e3481620f4b7a88
MPMarketing <gabriel_workspace@outlook.com>
2024-01-08 10:42:58 -0300

    Adjust GTM Server

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,4 +1,3 @@
-import ConsentBanner from "../Components/Cookies Consent";
 import TrueFooter from "../Components/Footers";
 import { ImageWrap1 } from "../Components/Image Wrappers/styles";
 import Header from "../Components/NavHead";
@@ -18,7 +17,6 @@ const Home = () => {
         <ImageWrap1 src="../../assets/james-miller.webp" alt="jamer-miller-signature"/>
       </WrapperB>
       <TrueFooter/>
-      {/* <ConsentBanner/> */}
     </>
     
   );
