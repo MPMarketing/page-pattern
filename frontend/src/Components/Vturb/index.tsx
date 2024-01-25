6cc8c545ed7a41e3bda1a05938976fa8da4156e7
MPMarketing <gabriel_workspace@outlook.com>
2024-01-25 11:10:18 -0300

    Adjust Vturb AB

--- frontend/src/Components/Vturb/index.tsx
+++ frontend/src/Components/Vturb/index.tsx
@@ -4,9 +4,10 @@ import { Diveturb } from './styles';
 const Vturb = () => {
     return(
         <Diveturb>
-          <div id="vid_6570e4f7b40e950008d151d4" style={{position: 'relative', width: '100%', padding: '56.25% 0 0'}}><img id="thumb_6570e4f7b40e950008d151d4" src="https://images.converteai.net/4fc732ef-52cc-458d-82c3-f067ec86c320/players/6570e4f7b40e950008d151d4/thumbnail.jpg" style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', objectFit: 'cover' , display: 'block'}} alt='vturb2'/><div id="backdrop_6570e4f7b40e950008d151d4" style={{position: 'absolute', top: '0', width: '100%', height: '100%', WebkitBackdropFilter: 'blur(5px)', backdropFilter: 'blur(5px)'}}></div></div>
+            <div dangerouslySetInnerHTML={{ __html: '<script type="text/javascript" id="scr_65b25d187d14870008a0e9be"></script>' }} />
             <Helmet>
-            <script type="text/javascript" id="scr_6570e4f7b40e950008d151d4">var s=document.createElement("script");s.src="https://scripts.converteai.net/4fc732ef-52cc-458d-82c3-f067ec86c320/players/6570e4f7b40e950008d151d4/player.js",s.async=!0,document.head.appendChild(s);</script>
+            <script type="text/javascript">var s=document.createElement("script");s.src="https://scripts.converteai.net/4fc732ef-52cc-458d-82c3-f067ec86c320/ab-test/65b25d187d14870008a0e9be/player.js",s.async=!0,document.head.appendChild(s);
+            </script>
             </Helmet>
         </Diveturb>
     )
