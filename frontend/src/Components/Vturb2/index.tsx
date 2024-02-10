19154ab06603e09198e4e8027bdb71a1afc2e989
MPMarketing <gabriel_workspace@outlook.com>
2024-02-09 23:47:39 -0300

    Adjust VSL True

--- frontend/src/Components/Vturb2/index.tsx
+++ frontend/src/Components/Vturb2/index.tsx
@@ -4,9 +4,12 @@ import { Diveturb } from './styles';
 const Vturb2 = () => {
     return(
         <Diveturb>
-        <div id="vid_65b25b295a96190009b24e69" style={{position: 'relative', width: '100%', padding: '56.25% 0 0'}}><img id="thumb_65b25b295a96190009b24e69" src="https://images.converteai.net/4fc732ef-52cc-458d-82c3-f067ec86c320/players/65b25b295a96190009b24e69/thumbnail.jpg" style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', objectFit: 'cover', display: 'block'}} alt=''/><div id="backdrop_65b25b295a96190009b24e69" style={{position: 'absolute', top: '0',width: '100%', height: '100%', backdropFilter: 'blur(5px)'}}></div></div>
+        <div id="vid_6570e4f7b40e950008d151d4" style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }}>
+        <img id="thumb_6570e4f7b40e950008d151d4" src="https://images.converteai.net/4fc732ef-52cc-458d-82c3-f067ec86c320/players/6570e4f7b40e950008d151d4/thumbnail.jpg" style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', objectFit: 'cover', display: 'block' }} alt='' />
+        <div id="backdrop_6570e4f7b40e950008d151d4" style={{ position: 'absolute', top: '0', width: '100%', height: '100%', backdropFilter: 'blur(5px)' }}></div>
+        </div>
         <Helmet>
-        <script type="text/javascript" id="scr_65b25b295a96190009b24e69">var s=document.createElement("script");s.src="https://scripts.converteai.net/4fc732ef-52cc-458d-82c3-f067ec86c320/players/65b25b295a96190009b24e69/player.js",s.async=!0,document.head.appendChild(s);</script>
+        <script type="text/javascript" id="scr_6570e4f7b40e950008d151d4">var s=document.createElement("script");s.src="https://scripts.converteai.net/4fc732ef-52cc-458d-82c3-f067ec86c320/players/6570e4f7b40e950008d151d4/player.js",s.async=!0,document.head.appendChild(s);</script>
         </Helmet>
         </Diveturb>
     )
